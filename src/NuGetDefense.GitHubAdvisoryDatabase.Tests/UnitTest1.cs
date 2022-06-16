7125684db474f3998d5746bc1f600f00d690e858
Curtis Carter <lokiphasma@gmail.com>
2022-06-15 21:30:47 -0500

    v2.0.0

--- src/NuGetDefense.GitHubAdvisoryDatabase.Tests/UnitTest1.cs
+++ src/NuGetDefense.GitHubAdvisoryDatabase.Tests/UnitTest1.cs
@@ -18,9 +18,18 @@ namespace GithubAdvisoryDatabaseClientTests
         [Fact]
         public void Test1()
         {
-            var scanner = new Scanner("TestFile.csproj", "Put Your Access Token Here");
+            var scanner = new Scanner("TestFile.csproj", "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");
             var results = scanner.GetVulnerabilitiesForPackages(InvulnerablePackages);
             Assert.True(results.Count == 1);
+
+        }
+
+        [Fact]
+        public void ToNuGetRangeTest()
+        {
+            const string range = ">= 4.6.0, < 4.7.2";
+            var ranges = Scanner.ToNugetRange(range);
+            Assert.True(ranges.Length == 2);
 
         }
     }
